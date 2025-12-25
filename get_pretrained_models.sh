404297b143e3533c092baa34097f2e34ca36179f
Ishi Vijayvergiya <iwnt164h@mailhub.rrze.uni-erlangen.de>
2025-12-25 18:49:32 +0100

    Adding files

--- get_pretrained_models.sh
+++ get_pretrained_models.sh
@@ -1,8 +1,19 @@
+# #!/usr/bin/env bash
+# #
+# # For licensing see accompanying LICENSE file.
+# # Copyright (C) 2024 Apple Inc. All Rights Reserved.
+# #
+# mkdir -p checkpoints
+# # Place final weights here:
+# wget https://ml-site.cdn-apple.com/models/depth-pro/depth_pro.pt -P checkpoints
+
+
+
 #!/usr/bin/env bash
 #
 # For licensing see accompanying LICENSE file.
 # Copyright (C) 2024 Apple Inc. All Rights Reserved.
 #
-mkdir -p checkpoints
+mkdir -p /home/hpc/iwnt/iwnt164h/checkpoint
 # Place final weights here:
-wget https://ml-site.cdn-apple.com/models/depth-pro/depth_pro.pt -P checkpoints
+wget https://ml-site.cdn-apple.com/models/depth-pro/depth_pro.pt -O /home/hpc/iwnt/iwnt164h/checkpoint/ml_depth_pro.pt
